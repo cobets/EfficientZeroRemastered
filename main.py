4cf3a0e59a515f4f021f46347919865ae9ccfb3c
Cobets Serhii <cobets@csltd.com.ua>
2023-09-04 19:46:05 +0300

    introducing tictactoe

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     parser.add_argument('--env', required=True, help='Name of the environment')
     parser.add_argument('--result_dir', default=os.path.join(os.getcwd(), 'results'),
                         help="Directory Path to store results (default: %(default)s)")
-    parser.add_argument('--case', required=True, choices=['atari'],
+    parser.add_argument('--case', required=True, choices=['atari', 'tictactoe'],
                         help="It's used for switching between different domains(default: %(default)s)")
     parser.add_argument('--opr', required=True, choices=['train', 'test'])
     parser.add_argument('--amp_type', required=True, choices=['torch_amp', 'none'],
@@ -73,6 +73,8 @@ if __name__ == '__main__':
     # import corresponding configuration , neural networks and envs
     if args.case == 'atari':
         from config.atari import game_config
+    elif args.case == 'tictactoe':
+        from config.tictactoe import game_config
     else:
         raise Exception('Invalid --case option')
 
